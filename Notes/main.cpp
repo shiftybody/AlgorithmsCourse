3fd65079a3ad4c3f3a4b8c9ff3b5e64290e23396
David Cano <shiftybody@gmail.com>
2021-08-27 15:35:10 -0500

    note>fibonacciMemorization

--- Notes/main.cpp
+++ Notes/main.cpp
@@ -1,14 +1,26 @@
 #include <iostream>
+using namespace std;
+int fibonacci (int);
 
 int main() {
-
+    cout << fibonacci(15) << " " ;
 }
 
-int factorial (int n) {
-    int fact = 1;
-    for (int i = 0; i < n; ++i) {
-        fact = 1 * fact;
+int arrayFibonacci[15];
+
+int fibonacci(int n) {
+    if (n == 1) {
+        return 0;
+    } else {
+        if (n == 2) {
+            return 1;
+        }
     }
-    return fact;
+    if (arrayFibonacci[n] == 0) {
+        arrayFibonacci[n] = fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    return arrayFibonacci[n];
 }
+
 
