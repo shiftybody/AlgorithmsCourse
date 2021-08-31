17d2128d40f6a3590d084d6730d2f8e385fdaa6f
David Cano <shiftybody@gmail.com>
2021-08-31 13:34:21 -0500

    edit TorresHanoi

--- TorresHanoi/main.cpp
+++ TorresHanoi/main.cpp
@@ -1,28 +1,21 @@
 #include <iostream>
 using namespace std;
-void hanoi (int, char, char, char);
 
-int main()
-{
+void hanoi(int, char, char, char);
+
+int main() {
     int n;
-    char auxiliar,B,C;
-
-    cin>>n;
-    hanoi(n,'A','C','B');
-
+    cin >> n;
+    hanoi(n, 'A', 'C', 'B');
 }
 
-void hanoi(int n,char A,char C,char B)
-{
-    if(n ==1 )
-    {
-        cout<<"Mover el disco "<< n<<" de "<<A<<" a "<<C<<endl;
-    }
-    else
-    {
-        hanoi(n-1,A,B,C);
-        cout<<"Mover el disco "<<n<<" de "<<A<<" a "<<C<<endl;
-        hanoi(n-1,B,C,A);
+void hanoi(int n, char origen, char destino, char auxiliar) {
+    if (n == 1) {
+        cout << "Mueve el disco " << n << " de " << origen << " a " << destino << endl;
+    } else {
+        hanoi(n - 1, origen, auxiliar, destino);
+        cout << "Mueve el disco " << n << " de " << origen << " a " << destino << endl;
+        hanoi(n - 1, auxiliar, destino, origen);
     }
 }
 
