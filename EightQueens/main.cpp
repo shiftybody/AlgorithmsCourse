781f54c72e69850101d72878e765ea0093b2924c
David Cano <shiftybody@gmail.com>
2021-09-10 11:32:23 -0500

    problema parcial

--- EightQueens/main.cpp
+++ EightQueens/main.cpp
@@ -3,29 +3,29 @@
 using namespace std;
 #define N 8
 
-
 void imprimirTablero(int[][N]);
+void resolverTablero(int[][N], int);
+bool valido(int[][N], int, int);
+void nuevoTablero(int[][N]);
 
-bool resolverTablero(int[][N]);
+int  contadorSoluciones = 0;
 
 int main() {
-    int tablero[N][N];
 
+    int tablero[N][N];
+    nuevoTablero(tablero);
+
+    resolverTablero(tablero, 0);
+    cout << endl << "El total de soluciones posibles es:" << contador ;
+
+}
+
+void nuevoTablero(int tablero[][N]) {
     for (int fila = 0; fila < N; fila++) {
         for (int columna = 0; columna < N; columna++) {
             tablero[fila][columna] = 0;
         }
     }
-
-    if (resolverTablero(tablero)) {
-        imprimirTablero(tablero);
-    } else cout << "No se puede resolver";
-
-}
-
-bool resolverTablero(int [][N] int col) {
-
-    return false;
 }
 
 void imprimirTablero(int tablero[][N]) {
@@ -36,6 +36,46 @@ void imprimirTablero(int tablero[][N]) {
         cout << endl;
     }
 }
+
+void resolverTablero(int tablero[][N], int col) {
+
+    for (int fila = 0; fila < N; fila++) {
+        if (valido(tablero, fila, col)) {
+
+            tablero[fila][col] = 1;
+
+            if (col < N-1) {
+                resolverTablero(tablero, col + 1);
+            } else {
+                imprimirTablero(tablero);
+                contadorSoluciones ++;
+                cout << endl;
+            }
+            tablero[fila][col] = 0;
+        }
+    }
+}
+
+bool valido(int tablero[][N], int fila, int col) {
+
+    for (int i = 0; i < col; i++)
+        if (tablero[fila][i])
+            return false;
+
+        /* Check upper diagonal on left side */
+        for (int i = fila, j = col; i >= 0 && j >= 0; i--, j--)
+            if (tablero[i][j])
+                return false;
+
+            /* Check lower diagonal on left side */
+            for ( int i = fila, j = col; j >= 0 && i < N; i++, j--)
+                if (tablero[i][j])
+                    return false;
+
+    return true;
+}
+
+bool
 
 
 
